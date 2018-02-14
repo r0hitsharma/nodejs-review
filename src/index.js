4141b02694804f06f9fbcfa476f22e54b9c2122e
r0hitsharma <rs@rohit.sh>
2018-02-14 21:04:52 +0100

    Added logging middleware.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ app.listen(port, () => {
 	console.log(`App is listening at port: ${ port }`);
 });
 
+// logging middleware
+app.use((req, res, next) => {
+	console.log(req.method, req.url);
+	next();
+});
+
 app.get("/", (req, res) => {
 	res.send("Welcome to our server!");
 });
